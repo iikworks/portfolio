74601e3710b82981f5e4a4cdad3308e0f8f6174a
Igor Karplyuk <johndevice834@gmail.com>
2022-09-26 18:22:51 +0300

    unpack database url

--- app/functions/config.php
+++ app/functions/config.php
@@ -8,4 +8,20 @@ function load_config(string $path): void
     foreach ($config as $key => $var) {
         if(!isset($_ENV['CONFIG_'.$key])) $_ENV['CONFIG_'.$key] = $var;
     }
+
+    unpack_database_url();
+}
+
+function unpack_database_url(): void
+{
+    if(isset($_ENV['DATABASE_URL'])){
+        $match = [];
+        preg_match('~^mysql://(?P<user>.+):(?P<password>.+)@(?P<host>.+)/(?P<name>.+)\?reconnect=true$~', $_ENV['DATABASE_URL'], $match);
+
+        foreach ($match as $key => $var) {
+            if($key != 0 && intval($key) == 0) {
+                $_ENV['CONFIG_DB_'.strtoupper($key)] = $var;
+            }
+        }
+    }
 }
