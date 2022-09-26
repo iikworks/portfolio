d4b5cb4a5b2601e6a1846c59d96cb54cfb82bc9b
Игорь Карплюк <johndevice834@gmail.com>
2022-09-26 10:29:06 +0300

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(!defined('APP_VERSION')) die('access denied');
-
-return [
-    'DB_HOST' => 'localhost',
-    'DB_USER' => 'root',
-    'DB_PASSWORD' => '121241',
-    'DB_NAME' => 'bonuses',
-];
